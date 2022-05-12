eef9b3fa5fa12fa5e1a07fcbdd5ed77a3d163636
Alexander Davis <alexander.davis.098@gmail.com>
2022-05-12 19:05:10 -0300

    Fix header

--- includes/header.php
+++ includes/header.php
@@ -21,13 +21,13 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <title>Jobs</title>
+  <title>Vagas</title>
 </head>
 
 <body class="bg-dark text-light">
   <div class="container">
     <div class="text-center">
-      <h1>Jobs</h1>
+      <h1>Vagas de emprego</h1>
       <p>Exemplo de CRUD com PHP orientado a Objetos</p>
 
       <hr class="border-light">
